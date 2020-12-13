31729a00d3ccc398525afe1d056e43289006a596
John Jegede <jegede400@gmail.com>
2020-12-12 21:42:20 -0500

    created the local storage

--- schedular.js
+++ schedular.js
@@ -35,91 +35,95 @@ setInterval(checkTime, 500);
 var textareaValue = $("textarea");
 console.log(textareaValue);
 
+var storeVal = {
+  nine: "",
+  ten: "",
+  eleven: "",
+  twelve: "",
+  one: "",
+  two: "",
+  three: "",
+  four: "",
+  five: "",
+};
 
 function checkTime() {
-  var recent =parseInt( moment.format("H") );
+  var recent = parseInt(moment.format("H"));
   //console.log(recent);
   var hour_list = $(".row");
   ///console.log(hour_list);
   hour_list.each(function () {
     var $value = $(this);
-    var $textarea = $value.find('textarea');
+    var $textarea = $value.find("textarea");
     var real = parseInt($textarea.attr("id"));
 
-    if(recent == real)
-    {
-        $textarea.addClass('present') ;
-    }else if (recent > real)
-    {
-        $textarea.addClass('past') ;
-    }else if (recent < real){
-        $textarea.addClass('future') 
+    if (recent == real) {
+      $textarea.addClass("present");
+    } else if (recent > real) {
+      $textarea.addClass("past");
+    } else if (recent < real) {
+      $textarea.addClass("future");
     }
-   // var real = $.attr("id");
+    // var real = $.attr("id");
     //console.log(real);
   });
 }
 
-  var $buttons = $(".saveBtn");
-  //console.log(textareaValue[1]);
-  //var text = textareaValue[0].value;
-      //console.log($buttons);
+var $buttons = $(".saveBtn");
+//console.log(textareaValue[1]);
+//var text = textareaValue[0].value;
+//console.log($buttons);
 
-  var storeVal = {"nine":"","ten":"","eleven":"","twelve":"","one":"","two":"","three":"","four":"","five":""};
+$buttons.on("click", function (e) {
+  e.preventDefault();
 
-  $buttons.on("click",function(e){
-      e.preventDefault();
+  ids = this.id;
+  var text;
 
-      ids = this.id;
-      var text;
-
-      if(ids === "nine")
-      {
-        text = textareaValue[0].value;
-        storeVal[ids] = text;
-        console.log(storeVal[ids]);
-
-      }else if (ids === "ten")
-      {
-        text = textareaValue[1].value;
-        storeVal[ids] = text;
-        console.log(storeVal[ids]);
-        //console.log(text);
-      }else if (ids === "eleven"){
-        text = textareaValue[2].value;
-        storeVal[ids] = text;
-        console.log(storeVal[ids]);
-      }else if (ids === "twelve"){
-        text = textareaValue[3].value;
-        storeVal[ids] = text;
-        console.log(storeVal[ids]);
-      }else if (ids === "one"){
-        text = textareaValue[4].value;
-        storeVal[ids] = text;
-        console.log(storeVal[ids]);
-      }else if (ids === "two"){
-        text = textareaValue[5].value;
-        storeVal[ids] = text;
-        console.log(storeVal[ids]);
-      }else if (ids === "three"){
-        text = textareaValue[6].value;
-        storeVal[ids] = text;
-        console.log(storeVal[ids]);
-      }else if (ids === "four"){
-        text = textareaValue[7].value;
-        storeVal[ids] = text;
-        console.log(storeVal[ids]);
-      }else if (ids === "five"){
-        text = textareaValue[8].value;
-        storeVal[ids] = text;
-        console.log(storeVal[ids]);
-      }
-
-     
-
-   // var value = 
-    
-  });
+  if (ids === "nine") {
+    text = textareaValue[0].value;
+    storeVal[ids] = text;
+    console.log(storeVal[ids]);
+  } else if (ids === "ten") {
+    text = textareaValue[1].value;
+    storeVal[ids] = text;
+    console.log(storeVal[ids]);
+    //console.log(text);
+  } else if (ids === "eleven") {
+    text = textareaValue[2].value;
+    storeVal[ids] = text;
+    console.log(storeVal[ids]);
+  } else if (ids === "twelve") {
+    text = textareaValue[3].value;
+    storeVal[ids] = text;
+    console.log(storeVal[ids]);
+  } else if (ids === "one") {
+    text = textareaValue[4].value;
+    storeVal[ids] = text;
+    console.log(storeVal[ids]);
+  } else if (ids === "two") {
+    text = textareaValue[5].value;
+    storeVal[ids] = text;
+    console.log(storeVal[ids]);
+  } else if (ids === "three") {
+    text = textareaValue[6].value;
+    storeVal[ids] = text;
+    console.log(storeVal[ids]);
+  } else if (ids === "four") {
+    text = textareaValue[7].value;
+    storeVal[ids] = text;
+    console.log(storeVal[ids]);
+  } else if (ids === "five") {
+    text = textareaValue[8].value;
+    storeVal[ids] = text;
+    console.log(storeVal[ids]);
+  }
 
   localStorage.setItem("storeVal", JSON.stringify(storeVal));
+  // var value =
+});
+
+var valStored = JSON.parse(localStorage.getItem("storeVal"));
+console.log(valStored);
+
 
